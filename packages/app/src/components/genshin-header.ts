5b4071e09b731fc1dc49b1df3563366d797845e4
Anamooz <pthynktwice@gmail.com>
2024-12-03 09:35:51 -0800

    added username being clickable

--- packages/app/src/components/genshin-header.ts
+++ packages/app/src/components/genshin-header.ts
@@ -1,13 +1,13 @@
 import { Auth, Observer, Events } from "@calpoly/mustang";
 import { css, html, LitElement } from "lit";
 import { state } from "lit/decorators.js";
-import { User } from "server/src/models/user.ts";
+import { User } from "server/models";
 import reset from "../styles/reset.css";
 
 function signOut(ev: MouseEvent) {
   Events.relay(ev, "auth:message", ["auth/signout"]);
+  window.location.href = "/login"; // Explicitly navigate to the absolute /login path
 }
-  
 
 export class GenshinHeaderElement extends LitElement {
   @state()
@@ -22,77 +22,86 @@ export class GenshinHeaderElement extends LitElement {
         <h1>Best Genshin Character Builds</h1>
         <p>Best weapons, artifacts, and skills and materials you'll need</p>
         <nav>
-          <a id="userLink" slot="actuator">
+          <a id="userLink" slot="actuator" @click=${this.navigateToUserPage}>
             Hello, <span id="userid" class="username">${this.username}</span>
           </a>
           <menu>
-          <li class="when-signed-in">
+            <li class="when-signed-in">
               <a id="signout" @click=${signOut}>Sign Out</a>
             </li>
-          <li class="when-signed-out">
-            <a href="/login">Sign In</a>
-          </li>
+            <li class="when-signed-out">
+              <a href="/login">Sign In</a>
+            </li>
           </menu>
         </nav>
       </header>
     `;
   }
 
+  navigateToUserPage() {
+    if (this.username && this.username !== "traveler") {
+      const url = `/app/${this.username}`;
+      window.location.href = url; // Redirect to the user's page
+    } else {
+      console.warn("Username is invalid or not set");
+    }
+  }
+
   static styles = [
     reset.styles,
     css`
-    :host {
-      display: contents;
-    }
+      :host {
+        display: contents;
+      }
 
-    header {
-      justify-content: space-between;
-      background-color: var(--color-background-page);
-      color: var(--color-text);
-      padding-top: 25px;
-    }
+      header {
+        justify-content: space-between;
+        background-color: var(--color-background-page);
+        color: var(--color-text);
+        padding-top: 25px;
+      }
 
-    header p {
-      font-size: 28px;
-      margin-left: 40px;
-    }
+      header p {
+        font-size: 28px;
+        margin-left: 40px;
+      }
 
-    header h1 {
-      font-size: 50px;
-      margin-left: 40px;
-    }
+      header h1 {
+        font-size: 50px;
+        margin-left: 40px;
+      }
 
-    nav {
-      display: flex;
-      flex-direction: column;
-      align-items: end;
-      margin-right: 20px;
-    }
+      nav {
+        display: flex;
+        flex-direction: column;
+        align-items: end;
+        margin-right: 20px;
+      }
 
-    a[slot="actuator"] {
-      color: var(--color-link-inverted);
-      cursor: pointer;
-      text-decoration: none;
-    }
+      a[slot="actuator"] {
+        color: var(--color-link-inverted);
+        cursor: pointer;
+        text-decoration: none;
+      }
 
-    .username {
-      text-decoration: underline;
-    }
+      .username {
+        text-decoration: underline;
+      }
 
-    #userid:empty::before {
-      content: "traveler";
-    }
+      #userid:empty::before {
+        content: "traveler";
+      }
 
-    menu a {
-      color: var(--color-link);
-      cursor: pointer;
-      text-decoration: underline;
-    }
-    a:has(#userid:empty) ~ menu > .when-signed-in,
-    a:has(#userid:not(:empty)) ~ menu > .when-signed-out {
-      display: none;
-    }
-  `
+      menu a {
+        color: var(--color-link);
+        cursor: pointer;
+        text-decoration: underline;
+      }
+      a:has(#userid:empty) ~ menu > .when-signed-in,
+      a:has(#userid:not(:empty)) ~ menu > .when-signed-out {
+        display: none;
+      }
+    `,
   ];
 
   hydrate(url: string) {
